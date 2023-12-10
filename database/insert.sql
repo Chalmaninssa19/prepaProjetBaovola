0566d287119865b383ba4d00c3cbb78446d5110a
Mandaniaina Luc <mandalucwwe@gmail.com>
2023-12-10 15:52:34 +0100

    1

---
+++ database/insert.sql
@@ -0,0 +1,86 @@
+INSERT INTO type_produit VALUES
+    (DEFAULT, 'Électronique'),
+    (DEFAULT, 'Vêtements'),
+    (DEFAULT, 'Meubles'),
+    (DEFAULT, 'Accessoires'),
+    (DEFAULT, 'Livres'),
+    (DEFAULT, 'Jouets'),
+    (DEFAULT, 'Sports'),
+    (DEFAULT, 'Nourriture'),
+    (DEFAULT, 'Beauté'),
+    (DEFAULT, 'Appareils ménagers');
+
+
+INSERT INTO categorie_produit VALUES
+    (DEFAULT, 'Smartphones'),
+    (DEFAULT, 'T-shirts'),
+    (DEFAULT, 'Sofas'),
+    (DEFAULT, 'Montres'),
+    (DEFAULT, 'Romans'),
+    (DEFAULT, 'Jouets'),
+    (DEFAULT, 'Sports'),
+    (DEFAULT, 'Collations'),
+    (DEFAULT, 'Maquillage'),
+    (DEFAULT, 'Électroménagers');
+
+INSERT INTO marque_produit VALUES
+    (DEFAULT, 'Samsung'),
+    (DEFAULT, 'Nike'),
+    (DEFAULT, 'IKEA'),
+    (DEFAULT, 'Rolex'),
+    (DEFAULT, 'Apple'),
+    (DEFAULT, 'LEGO'),
+    (DEFAULT, 'Adidas'),
+    (DEFAULT, 'Sony'),
+    (DEFAULT, 'L\'Oréal'),
+    (DEFAULT, 'LG');
+
+
+INSERT INTO genre VALUES
+    (DEFAULT, 'High-Tech'),
+    (DEFAULT, 'Mode'),
+    (DEFAULT, 'Mobilier'),
+    (DEFAULT, 'Accessoires'),
+    (DEFAULT, 'Littérature'),
+    (DEFAULT, 'Jouets'),
+    (DEFAULT, 'Sports'),
+    (DEFAULT, 'Alimentation'),
+    (DEFAULT, 'Produits de beauté'),
+    (DEFAULT, 'Électroménagers');
+
+INSERT INTO taille VALUES
+    (DEFAULT, 'Petit'),
+    (DEFAULT, 'Moyen'),
+    (DEFAULT, 'Grand'),
+    (DEFAULT, 'XL'),
+    (DEFAULT, 'XXL'),
+    (DEFAULT, 'S'),
+    (DEFAULT, 'M'),
+    (DEFAULT, 'L'),
+    (DEFAULT, 'Taille unique'),
+    (DEFAULT, '3XL');
+
+
+INSERT INTO couleur VALUES
+    (DEFAULT, 'Rouge'),
+    (DEFAULT, 'Bleu'),
+    (DEFAULT, 'Vert'),
+    (DEFAULT, 'Jaune'),
+    (DEFAULT, 'Noir'),
+    (DEFAULT, 'Rose'),
+    (DEFAULT, 'Violet'),
+    (DEFAULT, 'Gris'),
+    (DEFAULT, 'Orange'),
+    (DEFAULT, 'Marron');
+
+INSERT INTO produit VALUES
+    (DEFAULT, 1, 1, 1, 1, 1, 1, '2023-01-01', 'Écran 6,7 pouces', 'Smartphone haut de gamme'),
+    (DEFAULT, 2, 2, 2, 2, 2, 2, '2023-01-02', 'Tissu doux', 'T-shirt décontracté'),
+    (DEFAULT, 3, 3, 3, 3, 3, 3, '2023-01-03', 'Design moderne', 'Canapé spacieux'),
+    (DEFAULT, 4, 4, 4, 4, 4, 4, '2023-01-04', 'Boîtier en acier inoxydable', 'Montre élégante'),
+    (DEFAULT, 5, 5, 5, 5, 5, 5, '2023-01-05', 'Formule hydratante', 'Crème pour le visage'),
+    (DEFAULT, 6, 6, 6, 6, 6, 6, '2023-01-06', 'Best-seller', 'Roman captivant'),
+    (DEFAULT, 7, 7, 7, 7, 7, 7, '2023-01-07', 'Chaussure de running', 'Chaussures de sport'),
+    (DEFAULT, 8, 8, 8, 8, 8, 8, '2023-01-08', 'Matériau durable', 'Ballon de soccer'),
+    (DEFAULT, 9, 9, 9, 9, 9, 9, '2023-01-09', 'Parfum floral', 'Parfum de luxe'),
+    (DEFAULT, 10, 10, 10, 10, 10, 10, '2023-01-10', 'Écran tactile', 'Tablette polyvalente');
